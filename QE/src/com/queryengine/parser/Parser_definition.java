888a673a3b2fa562ad200abcdf425b7f93ecc7a8
vigneshwaraarasu <vignes.89@gmail.com>
2016-05-08 21:13:27 +0530

    Revert "Query Engine"
    This reverts commit e9bf3153c110ade64c113335c44d0c55db699665.

--- QE/src/com/queryengine/parser/Parser_definition.java
+++
@@ -1,9 +0,0 @@
-/* interface for parser phase */
-package com.queryengine.parser;
-
-public interface Parser_definition {
-	public boolean syntax_checker(String query);
-
-	public Queryparser keyword_identifier(String query);
-
-}
